6e20296a7e6ac27660ca422f87683461ca256116
Kulgar <regmillet@ct2c.fr>
2022-05-31 15:09:28 +0200

    Can reactivate PaymentMethodDecorator code thanks to quick fix https://github.com/mrbrdo/spree_mobility/issues/7

--- lib/spree_mobility/core_ext/spree/payment_method_decorator.rb
+++ lib/spree_mobility/core_ext/spree/payment_method_decorator.rb
@@ -1,10 +1,10 @@
 module SpreeMobility::CoreExt::Spree::PaymentMethodDecorator
   def self.prepended(base)
-    # base.include SpreeMobility::Translatable
-    # SpreeMobility.translates_for base, :name, :description
+    base.include SpreeMobility::Translatable
+    SpreeMobility.translates_for base, :name, :description
 
-    # base.translation_class.class_eval do
-    #   validates :name, presence: true
-    # end
+    base.translation_class.class_eval do
+      validates :name, presence: true
+    end
   end
 end
